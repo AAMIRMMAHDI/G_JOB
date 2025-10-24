93c837894dbf82270dc78e297c959b318309d033
AAMIRMMAHDI <156841411+AAMIRMMAHDI@users.noreply.github.com>
2025-10-24 13:02:28 +0330

    CLONE_5

--- send/sitemaps.py
+++ send/sitemaps.py
@@ -1,11 +1,13 @@
-# send/sitemaps.py
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 from .models import Business
+from datetime import datetime
 
 class StaticSitemap(Sitemap):
     priority = 0.8
     changefreq = 'weekly'
+    protocol = 'http'
+    domain = '127.0.0.1:8000'  # چون رو لوکال‌هاست تست می‌کنی
 
     def items(self):
         return [
@@ -18,12 +20,20 @@ class StaticSitemap(Sitemap):
     def location(self, item):
         return reverse(item)
 
+    def lastmod(self, item):
+        return datetime.now()
+
 class BusinessSitemap(Sitemap):
-    changefreq = "daily"
+    changefreq = 'daily'
     priority = 0.9
+    protocol = 'http'
+    domain = '127.0.0.1:8000'
 
     def items(self):
         return Business.objects.filter(is_approved=True).order_by('-created_at')
 
     def location(self, obj):
         return obj.get_absolute_url()
+
+    def lastmod(self, obj):
+        return obj.created_at
